57c8cfc22206b75ac9a1374e6a52b362677969a9
ice-bear <jueomgim202@gmail.com>
2025-04-08 23:26:48 +0900

    틀만 완성...

--- src/test/java/com/ncmn/school/SchoolApplicationTests.java
+++ src/test/java/com/ncmn/school/SchoolApplicationTests.java
@@ -1,13 +1,19 @@
 package com.ncmn.school;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class SchoolApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
