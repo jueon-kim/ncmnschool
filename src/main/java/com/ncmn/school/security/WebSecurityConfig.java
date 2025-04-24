f42383d2d6994a50746a048e7537a76b31944ae0
ice-bear <jueomgim202@gmail.com>
2025-04-24 23:25:40 +0900

    더 자세한 계획이 필요함

--- src/main/java/com/ncmn/school/security/WebSecurityConfig.java
+++ src/main/java/com/ncmn/school/security/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/", "/members/join", "/home").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
--- src/main/java/com/ncmn/school/web/MemberController.java
+++ src/main/java/com/ncmn/school/web/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,18 +19,32 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @PostMapping("/members")
+    // 관리자 컨트롤러
+    @PostMapping("/admin/members")
     public String create(Member member) {
         System.out.println("회원 가입 요청: " + member.getName()); // 디버깅용 로그
         memberService.join(member);
-        return "redirect:/members"; // 가입 후 메인으로 리다이렉트
+        return "redirect:/home"; // 가입 후 메인으로 리다이렉트
     }
 
-    @GetMapping(value = "/members")
+    @GetMapping(value = "/admin/members")
     public String list(Model model) {
         List<Member> members = memberService.findAll();
         model.addAttribute("members", members);
-        return "members"; // members.html 렌더링
+        return "/admin/members"; // members.html 렌더링
+    }
+
+    // 회원 등록 컨트롤러
+    @GetMapping(value = "/home")
+    public String home(@RequestParam(name="home", required = false, defaultValue = "home") String home, Model model){
+        model.addAttribute("home", home);
+        return "/home";
+    }
+
+    @GetMapping("/member/join")
+    public String joinForm(){
+        return "member/join";
+
     }
 
 }
